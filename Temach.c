d1b51f24018313340ca0dbd292cf7fd5c4ac729d
FreakJLH2025 <joshua.luja@gmail.com>
2025-12-16 16:09:41 +0000

    Temach.c

---
+++ Temach.c
@@ -0,0 +1,18 @@
+/*
+* File: simple.c
+* Auth: Joshua
+* Desc: A simple program that prints a message.
+*/
+
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("Hello, Betty!\n");
+return (0);
+}

a893d84bd4fc04eb5cca85c89afbae8a4f12132c
FreakJLH2025 <joshua.luja@gmail.com>
2025-12-18 20:09:09 +0000

    prompt.c

---
+++ prompt.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "shell.h"
+
+/**
+* display_prompt - Prints the shell prompt
+*
+* Return: Nothing
+*/
+void display_prompt(void)
+{
+printf("#cisfun$ ");
+fflush(stdout);
+}

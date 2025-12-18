c27effc6ddb2df9151e804be1f35a6279d2332fb
FreakJLH2025 <joshua.luja@gmail.com>
2025-12-18 20:19:35 +0000

    shell.h

--- shell.h
+++ shell.h
@@ -7,5 +7,6 @@ extern char **environ;
 void display_prompt(void);
 char *read_line(void);
 void execute_command(char *line);
-
+char *trim_spaces(char *str);
+char **split_line(char *line);
 #endif /* SHELL_H */
